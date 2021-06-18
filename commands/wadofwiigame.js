bb05d01b72e04b2de1d373bb42d88b57f1f61ffc
METHOD_MUSIC <zacattak03@gmail.com>
2021-06-17 20:13:40 -0700

    fixed idocracy

--- commands/wadofwiigame.js
+++ commands/wadofwiigame.js
@@ -8,6 +8,10 @@ module.exports = {
     requiredRoles: [],
     description: "When someone needs to shut the fuck up",
     callback: (message, arguments, text, generalDictionary, wiiDictionary, wiiuDictionary, DSDictionary, wadErrorCodes) => {
-        return message.channel.send(`${message.mentions.users.first()} ${generalDictionary.shutup}`)
+        if(arguments[1]) {
+            return message.channel.send(`${message.mentions.users.first()} ${generalDictionary.shutup}`);
+        } else {
+            return message.channel.send(`${generalDictionary.shutup}`);
+        }
     }
 }
