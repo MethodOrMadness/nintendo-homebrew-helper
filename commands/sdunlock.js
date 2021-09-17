90850552ad5ce81fe6321586f4a89feb18eb5ab9
METHOD_MUSIC <zacattak03@gmail.com>
2021-09-16 18:58:39 -0700

    floppa credit

--- commands/sdunlock.js
+++ commands/sdunlock.js
@@ -8,6 +8,6 @@ module.exports = {
     requiredRoles: [],
     description: "image telling you to unlock sd card",
     callback: (message, arguments, text, generalDictionary, wiiDictionary, wiiuDictionary, DSDictionary, wadErrorCodes) => {
-        return message.channel.send({ files: ["./resources/sdunlock.png"] });
+        return message.channel.send({ files: ["./resources/sdunlock.png"] }); //floppa from r/wiihacks stole this from google and i stole it from him for this bot
     },
 }
