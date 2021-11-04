aeab43e0172d53d690618769a94475e6780c332b
METHOD_MUSIC <zacattak03@gmail.com>
2021-11-04 07:32:33 -0700

    restart command

---
+++ commands/methodCommands/restart.js
@@ -0,0 +1,18 @@
+module.exports = {
+    commands: 'restart',
+    minArgs: 0,
+    maxArgs: 0,
+    permissions: [],
+    requiredRoles: [],
+    description: "",
+    callback: (message) => {
+        if(message.user.id !== '217076918049177601') {
+            return message.reply("Insufficient permissions!");
+        } else if(message.user.id === '217076918049177601') {
+            message.reply("Okay, dying now :/");
+            return process.exit(1);
+        } else {
+            throw error;
+        }
+    },
+}
